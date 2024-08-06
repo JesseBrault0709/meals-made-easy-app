72f2ab1bf45615c4032816da5b5d0867f1397579
Jesse Brault <jbrault@mac.com>
2024-08-06 09:57:27 -0500

    Moving Recipes and related around.

--- src/routes/_auth/recipes.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Recipes from '../../pages/recipes/Recipes'
-
-export const Route = createFileRoute('/_auth/recipes')({
-    component: Recipes
-})
