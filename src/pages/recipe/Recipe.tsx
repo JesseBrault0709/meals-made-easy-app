d4686d0a39cc705d6d50503f5c4d604ea8b9f11e
Jesse Brault <jbrault@mac.com>
2024-08-10 18:04:34 -0500

    Some styling of RecipePage.

--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -13,15 +13,17 @@ const Recipe = ({ recipe, imgUrl }: RecipeProps) => {
     return (
         <div className={classes.fullRecipeContainer}>
             <article className={classes.fullRecipe}>
+                <div className={classes.info}>
+                    <div className={classes.infoRow}>
+                        <h1 className={classes.recipeTitle}>{recipe.title}</h1>
+                        <StarCount count={recipe.starCount} />
+                    </div>
+                    <div className={classes.infoRow}>
+                        <UserIconAndName username={recipe.ownerUsername} />
+                        <RecipeVisibilityIcon isPublic={recipe.isPublic} />
+                    </div>
+                </div>
                 <img src={imgUrl} className={classes.mainImage} />
-                <div className={classes.infoRow}>
-                    <h1>{recipe.title}</h1>
-                    <StarCount count={recipe.starCount} />
-                </div>
-                <div className={classes.infoRow}>
-                    <UserIconAndName username={recipe.ownerUsername} />
-                    <RecipeVisibilityIcon isPublic={recipe.isPublic} />
-                </div>
                 <div dangerouslySetInnerHTML={{ __html: recipe.text }} />
             </article>
         </div>
