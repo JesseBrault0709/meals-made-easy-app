d033ee5589ed0f60efe810ca4a94837522d2aed8
Jesse Brault <jbrault@mac.com>
2024-08-14 07:38:18 -0500

    Added vite-plugin-checker.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
+    plugins: [react(), TanStackRouterVite(), checker({ typescript: true })],
     css: {
         modules: {
             localsConvention: 'camelCaseOnly'
